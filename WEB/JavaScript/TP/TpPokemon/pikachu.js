840f2057af2b1799d927c503e51d5f1b64e066bf
Jordan <Jordanabid@gmail.com>
2023-03-01 16:27:05 +0100

    pika pika

--- WEB/JavaScript/TP/TpPokemon/pikachu.js
+++ WEB/JavaScript/TP/TpPokemon/pikachu.js
@@ -4,43 +4,83 @@
 //4) Modifier la position de la div pikachu en fonction de la direction (+-30px par deplacement) et changer l'image
 //5) Verifier que pikachu ne sort pas de la div grass
 
-  var posX=0;
-  var posY=0;
+var posX=0;
+var posY=0;
 
-  var pokemon="pikachu";
-  var img="Down.png";
+var pokemon="pikachu";
+var img="Down.png";
 
+imgPikachu.setAttribute("src","assets/img/"+pokemon+img);
+
+inputName.onkeyup = verifName;
+btnStart.onclick=lancerAventure;
+
+
+function verifName(event)
+{
+  if(inputName.value!="")
+  {
+    btnStart.disabled=false;
+    if(event.key=="Enter")
+    {
+      lancerAventure();
+    }
+  }
+  else
+  {
+    btnStart.disabled=true;
+  }
+}
+
+function lancerAventure()
+{
+  grass.style.display="block";
+  formStart.style.display="none";
+  pikachu.setAttribute("title",inputName.value);
+  //theme.play();
+  document.body.onkeyup=deplacement;
+}
+
+function deplacement(event)
+{
+  if(event.key=="ArrowDown" || event.key=="s" )
+  {
+    if(posY<=600)
+    {
+      posY+=30;
+    }
+    img="Down.png";
+  }
+  else if(event.key=="ArrowRight" || event.key=="d" )
+  {
+    if(posX<=600)
+    {
+      posX+=30;
+    }
+    img="Right.png";
+  }
+
+  else if(event.key=="ArrowLeft" || event.key=="q")
+  {
+    if(posX>=30)
+    {
+      posX-=30;
+    }
+    img="Left.png";
+  }
+
+  else if(event.key=="ArrowUp" || event.key=="z")
+  {
+    if(posY>=30)
+    {
+      posY-=30;
+    }
+    img="Up.png";
+  }
+
+
+  pikachu.style.top=posY+"px";
+  pikachu.style.left=posX+"px";
   imgPikachu.setAttribute("src","assets/img/"+pokemon+img);
 
-  function lancerAventure()
-  {
-
-  }
-
-  function deplacement(event)
-  {
-    if(event.key=="ArrowDown" || event.key=="s" )
-    {
-
-    }
-    else if(event.key=="ArrowRight" || event.key=="d" )
-    {
-
-    }
-
-    else if(event.key=="ArrowLeft" || event.key=="q")
-    {
-
-    }
-
-    else if(event.key=="ArrowUp" || event.key=="z")
-    {
-
-    }
-
-
-    pikachu.style.top=posY+"px";
-    pikachu.style.left=posX+"px";
-    imgPikachu.setAttribute("src","assets/img/"+pokemon+img);
-
-  }
+}
