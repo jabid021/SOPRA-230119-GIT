b28c291131f1f502040bca27d1cab1864f01b6e6
Jeremy <jeremy.perrouault@ascadis.fr>
2023-03-06 15:10:02 +0100

    JOUR 1 - INTRO SPRING CORE - Correction exo Application dans contexte de SPRING

--- SPRING-CORE/src/main/java/fr/formation/Application.java
+++ SPRING-CORE/src/main/java/fr/formation/Application.java
@@ -1,12 +1,25 @@
 package fr.formation;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import fr.formation.config.AppConfig;
 import fr.formation.musicien.Guitariste;
 import fr.formation.musicien.Pianiste;
 
+//@Component
 public class Application {
+	@Autowired
+	private Guitariste guitariste;
+	
+	@Autowired
+	private Pianiste pianiste;
+	
+	public void run() {
+		this.guitariste.jouer();
+		this.pianiste.jouer();
+	}
+	
 	public static void main(String[] args) {
 //		Guitariste guitariste = new Guitariste();
 //		
@@ -18,17 +31,24 @@ public class Application {
 		
 		
 		// Configuration JAVA (Classe AppConfig)
-		AnnotationConfigApplicationContext context =
-			new AnnotationConfigApplicationContext(AppConfig.class);
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+		
+		// On peut créer une instance manangée à SPRING directement à partir du contexte
+		Application app = context.getBeanFactory().createBean(Application.class);
+		
+		// On extrait Application du contexte de SPRING
+//		Application app = context.getBean(Application.class);
+		app.run();
+		
 		
 		// On extrait le guitariste du contexte de SPRING
-		Guitariste guitariste = context.getBean(Guitariste.class);
-		
-		guitariste.jouer();
-		
-		// On extrait le pianiste du contexte de SPRING
-		Pianiste pianiste = context.getBean(Pianiste.class);
-		
-		pianiste.jouer();
+//		Guitariste guitariste = context.getBean(Guitariste.class);
+//		
+//		guitariste.jouer();
+//		
+//		// On extrait le pianiste du contexte de SPRING
+//		Pianiste pianiste = context.getBean(Pianiste.class);
+//		
+//		pianiste.jouer();
 	}
 }
