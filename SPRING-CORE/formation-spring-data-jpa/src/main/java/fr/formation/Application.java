68c80ce4fb2522f77a2d46486bc5d130092b83af
Jeremy <jeremy.perrouault@ascadis.fr>
2023-03-07 15:21:39 +0100

    JOUR 2 - INIT PROJET SPRING DATA-JPA & DEMO OPTIONAL

---
+++ SPRING-CORE/formation-spring-data-jpa/src/main/java/fr/formation/Application.java
@@ -0,0 +1,30 @@
+package fr.formation;
+
+import java.util.Optional;
+
+import fr.formation.model.Produit;
+
+public class Application {
+
+	public static void main(String[] args) {
+//		Produit produit = null;
+		Produit simulationProduit = new Produit(100);
+		
+//		Optional<Produit> optProduit = Optional.empty();
+		Optional<Produit> optProduit = Optional.of(simulationProduit);
+		
+		if (optProduit.isPresent()) {
+			System.out.println("ICI " + optProduit.get().getId());			
+		}
+		
+		else {
+			System.out.println("Le produit n'existe pas.");
+		}
+		
+		////
+		
+		Produit produit = optProduit.orElse(new Produit(5));
+		System.out.println("ET LA " + produit.getId());
+	}
+
+}
---
+++ SPRING-CORE/formation-spring-data-jpa/src/main/java/fr/formation/model/Produit.java
@@ -0,0 +1,18 @@
+package fr.formation.model;
+
+public class Produit {
+	private int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	
+	public Produit(int id) {
+		this.id = id;
+	}
+}
