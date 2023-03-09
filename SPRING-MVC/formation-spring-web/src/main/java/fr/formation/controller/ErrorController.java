836d087c1e1a19abefb2c99ee17d8b3f09c3b3a7
Jeremy <jeremy.perrouault@ascadis.fr>
2023-03-09 12:15:51 +0100

    JOUR 1 - Correction Page erreur

---
+++ SPRING-MVC/formation-spring-web/src/main/java/fr/formation/controller/ErrorController.java
@@ -0,0 +1,14 @@
+package fr.formation.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/erreur")
+public class ErrorController {
+	@GetMapping
+	public String afficherErreur() {
+		return "/WEB-INF/views/error.jsp";
+	}
+}
--- SPRING-MVC/formation-spring-web/src/main/java/fr/formation/controller/HomeController.java
+++ SPRING-MVC/formation-spring-web/src/main/java/fr/formation/controller/HomeController.java
@@ -4,20 +4,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import fr.formation.request.UserRequest;
 
 @Controller // Managée par SPRING en tant que Controller
+@RequestMapping("/accueil") // Préfixer tous les mappings de la classe
 public class HomeController {
 	
 	// On mappe la méthode sur la ressource HTTP "/accueil"
 //	@RequestMapping(value = "/accueil", method = RequestMethod.GET)
-	@GetMapping("/accueil")
+	@GetMapping
 	public String afficherAccueil() {
 		return "/WEB-INF/views/home.jsp";
 	}
 	
-	@PostMapping("/accueil")
+	@PostMapping
 //	public String seConnecter(@RequestParam String username, @RequestParam String password, Model model) {
 	public String seConnecter(UserRequest user, Model model) {
 		System.out.println("Le user " + user.getUsername() + " se connecte ...");
@@ -29,7 +31,8 @@ public class HomeController {
 		}
 		
 		else {
-			return "/WEB-INF/views/error.jsp";
+//			return "/WEB-INF/views/error.jsp";
+			return "redirect:/erreur";
 		}
 	}
 }
