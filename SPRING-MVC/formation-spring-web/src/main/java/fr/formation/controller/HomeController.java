cc4fbd9bbdd253ff5fba2ad8ed11208fcf835ac8
Jeremy <jeremy.perrouault@ascadis.fr>
2023-03-09 11:23:29 +0100

    JOUR 1 - PROJET INTRO SPRING MVC

---
+++ SPRING-MVC/formation-spring-web/src/main/java/fr/formation/controller/HomeController.java
@@ -0,0 +1,24 @@
+package fr.formation.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller // Managée par SPRING en tant que Controller
+public class HomeController {
+	
+	// On mappe la méthode sur la ressource HTTP "/accueil"
+//	@RequestMapping(value = "/accueil", method = RequestMethod.GET)
+	@GetMapping("/accueil")
+	public String afficherAccueil() {
+		return "/WEB-INF/views/home.jsp";
+	}
+	
+	@PostMapping("/accueil")
+	public String seConnecter(@RequestParam String username) {
+		System.out.println("Le user " + username + " se connecte ...");
+
+		return "/WEB-INF/views/home.jsp";
+	}
+}
---
+++ SPRING-MVC/formation-spring-web/src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8" />
+<title>Accueil</title>
+</head>
+<body>
+	<h1>Hello le monde !?</h1>
+	
+	
+	<form method="POST">
+		<input type="text" name="username" />
+		<input type="submit" value="OK !" />
+ 	</form>
+	
+
+</body>
+</html>
