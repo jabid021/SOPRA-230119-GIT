c6d502384b26ee217520b927931cb0fac555de6e
Jeremy <jeremy.perrouault@ascadis.fr>
2023-03-16 15:22:28 +0100

    JOUR 1 - USERDETAILS JPA

--- SPRING-BOOT-SECURITY/formation-spring-boot-security/src/main/java/fr/formation/config/JpaUserDetailsService.java
+++ SPRING-BOOT-SECURITY/formation-spring-boot-security/src/main/java/fr/formation/config/JpaUserDetailsService.java
@@ -1,21 +1,39 @@
 package fr.formation.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import fr.formation.dao.IUtilisateurDao;
+import fr.formation.model.Utilisateur;
+
 @Service
 public class JpaUserDetailsService implements UserDetailsService {
+	@Autowired
+	private IUtilisateurDao daoUtilisateur;
+	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		System.out.println(username);
+		Utilisateur utilisateur = this.daoUtilisateur
+			.findByUsername(username)
+			.orElseThrow(() -> new UsernameNotFoundException("Utilisateur pas trouvé."));
 		
-		return User
-				.withUsername("demo")
-				.password("123456")
-				.roles("USER")
-				.build();
+		UserBuilder userBuilder = User
+				.withUsername(username)
+				.password(utilisateur.getPassword());
+		
+		if (utilisateur.isAdmin()) {
+			userBuilder.roles("ADMIN");
+		}
+		
+		else {
+			userBuilder.roles("USER");
+		}
+		
+		return userBuilder.build();
 	}
 }
---
+++ SPRING-BOOT-SECURITY/formation-spring-boot-security/src/main/java/fr/formation/model/Utilisateur.java
@@ -0,0 +1,58 @@
+package fr.formation.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "utilisateur")
+public class Utilisateur {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+
+	@Column(name = "username", nullable = false)
+	private String username;
+
+	@Column(name = "password", nullable = false)
+	private String password;
+
+	@Column(name = "admin", nullable = false)
+	private boolean admin;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isAdmin() {
+		return admin;
+	}
+
+	public void setAdmin(boolean admin) {
+		this.admin = admin;
+	}
+}
