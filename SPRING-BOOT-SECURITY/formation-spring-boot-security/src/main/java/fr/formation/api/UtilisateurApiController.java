b5aebfe42bd590ac88bc708e941b649bd5dd2ac5
Jeremy <jeremy.perrouault@ascadis.fr>
2023-03-17 14:45:01 +0100

    JOUR 2 - CORS sur API UTILISATEUR

--- SPRING-BOOT-SECURITY/formation-spring-boot-security/src/main/java/fr/formation/api/UtilisateurApiController.java
+++ SPRING-BOOT-SECURITY/formation-spring-boot-security/src/main/java/fr/formation/api/UtilisateurApiController.java
@@ -5,6 +5,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ import fr.formation.response.AuthResponse;
 
 @RestController
 @RequestMapping("/api/utilisateur")
+@CrossOrigin("*")
 public class UtilisateurApiController {
 	@Autowired
 	private IUtilisateurDao daoUtilisateur;
